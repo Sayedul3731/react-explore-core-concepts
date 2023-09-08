7b01aa23e21aeef408eebc286b77fbbd300ffeb3
MD Sayedul Haque <mdsayedulhaque932@gmail.com>
2023-09-08 11:38:25 -0700

    added userid and id

--- src/Post.jsx
+++ src/Post.jsx
@@ -1,9 +1,11 @@
 import './Post.css'
 export default function Post({post}){
-    const {title, body} = post;
+    const {title, body, userId, id} = post;
     return (
         <div className="box">
             <h3>Title: {title} </h3>
+            <p> UserId: <small> {userId} </small> </p>
+            <p> Id: <small> {id} </small> </p>
             <p>Body: {body} </p>
         </div>
     )
